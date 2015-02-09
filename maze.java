05faa0787bca36f935cada3a0b22c442182f57eb
Albert Mokrejs <albertmokrejs@gmail.com>
2015-02-09 16:53:45 -0500

    Update maze.java

--- maze.java
+++ maze.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class mase{
+public class maze{
 
 public static void main(String[]args){
 char[][] a = new char[8][];
@@ -9,13 +9,19 @@ a[1] = new char[]{'X','s','X',' ','X'};
 a[2] = new char[]{'X',' ',' ',' ','X'};
 a[3] = new char[]{'X',' ','X',' ','X'};
 a[4] = new char[]{'X','X','X','E','X'};
-System.out.println(solver(a));}
+char[][] g = solver(a);
+for(int x = 0; x<a.length;x++){
+  String str = "";
+    for(int y = 0; y<a[0].length;y++){
+  str+= a[x][y];}
+  System.out.println(str + "\n");}}
 
 public static char[][] solver(char[][] a){
 for(int x = 0; x < a.length; x++){
 for(int y = 0; y < a[0].length; y++){
 if(a[x][y] == 's'){
-return solve(a,x,y);}}}}
+return solve(a,x,y);}}}
+return new  char[1][1];}
 
 public static char[][] solve(char[][] a, int b, int c){
 if(a[b][c] == 'E'){
@@ -25,11 +31,11 @@ return new char[1][1];}
 a[b][c] = '@';
 char[][] tmp = solve(a,b+1,c);
 if(tmp.length > 1){return tmp;}
-char[][] tmp = solve(a,b-1,c);
-if(tmp.length > 1){return tmp;}
-char[][] tmp = solve(a,b,c+1);
-if(tmp.length > 1){return tmp;}
-char[][] tmp = solve(a,b,c-1);
-if(tmp.length > 1){return tmp;}
+char[][] tmpa = solve(a,b-1,c);
+if(tmpa.length > 1){return tmpa;}
+char[][] tmpb = solve(a,b,c+1);
+if(tmpb.length > 1){return tmpb;}
+char[][] tmpc = solve(a,b,c-1);
+if(tmpc.length > 1){return tmpc;}
 return new char[1][1];}
 }
