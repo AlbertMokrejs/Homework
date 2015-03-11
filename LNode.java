d6d96b938e9b8c651be1db75170161687ff16298
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-10 20:28:17 -0400

    Update LNode.java

--- LNode.java
+++ LNode.java
@@ -11,12 +11,49 @@ public void link(LNode a){
 public int get(){
   return value;}
   
-public int next(){
-  return pointer;}
+public LNode next(){
+  if(hasNext()){
+  return pointer;}}
   
 public boolean hasNext(){
   return pointer!=null;}
   
+public void delink(){
+  pointer = null;}
+
+public int findEnd(){
+  if(hasNext()){
+    return 1+next().findEnd();}
+  return 0;}
+  
+public LNode findFromEnd(int n){
+  int x = findEnd();
+  LNode tmp = this;
+  while(x > n and tmp.hasNext()){
+    tmp = tmp.next();
+    x--;
+  }
+  return tmp;
+}
+
+public void insert(LNode a){
+  LNode x;
+  if(hasNext()){
+    x = next();
+  }
+  a.link(x);
+  link(a);
+}
+
+public void remove(int n){
+  if(n == 0){
+    findFromEnd(1).delink();
+  }
+  else{
+    findFromEnd(n+1).link(findFromEnd(n-1));
+  }
+}
+
 LNode(int a, LNode b){
   value = a;
   pointer = b;}
@@ -27,5 +64,5 @@ LNode(){
   
 LNode(int a){
   value = a;
-  pointer = null;}
+  pointer = null;}}
   
