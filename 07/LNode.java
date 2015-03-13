65cb8d25cd5872470bf895cd817cc6be9ed7020e
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-13 12:18:46 -0400

    Update LNode.java

--- 07/LNode.java
+++ 07/LNode.java
@@ -1,72 +1,56 @@
 public class LNode{
-int value;
-LNode pointer;
+    int value;
+    LNode pointer;
 
-public void set(int a){
-  value = a;}
+    public void set(int a){
+	value = a;}
 
-public void link(LNode a){
-  pointer = a;}
+    public void link(LNode a){
+	pointer = a;}
   
-public int get(){
-  return value;}
+    public int get(){
+	return value;}
   
-public LNode next(){
-  if(hasNext()){
-  return pointer;}}
+    public LNode next(){
+	if(hasNext()){
+	    return pointer;}
+	return null;}
   
-public boolean hasNext(){
-  return pointer!=null;}
+    public boolean hasNext(){
+	return pointer!=null;}
   
-public void delink(){
-  pointer = null;}
+    public void delink(){
+	pointer = null;}
 
-public int findEnd(){
-  LNode tmp = this;
-  int x = 0;
-  while(tmp.hasNext()){
-    tmp = tmp.next();
-    x++}
-  return x;
-  }
+    public int findEnd(){
+	if(hasNext()){
+	    return 1+next().findEnd();}
+	return 0;}
   
-public LNode findFromEnd(int n){
-  int x = findEnd();
-  LNode tmp = this;
-  while(x > n and tmp.hasNext()){
-    tmp = tmp.next();
-    x--;
-  }
-  return tmp;
+    public LNode findFromEnd(int n){
+	int x = findEnd();
+	LNode tmp = this;
+	while(x > n && tmp.hasNext()){
+	    tmp = tmp.next();
+	    x--;
+	}
+	return tmp;
+    }
+
+
+    public String toString(){
+	return ""+value;}
+
+    LNode(int a, LNode b){
+	value = a;
+	pointer = b;}
+  
+    LNode(){
+	value = 0;
+	pointer = null;}
+  
+    LNode(int a){
+	value = a;
+	pointer = null;}
+
 }
-
-public void insert(LNode a){
-  LNode x;
-  if(hasNext()){
-    x = next();
-  }
-  a.link(x);
-  link(a);
-}
-
-public void remove(int n){
-  if(n == 0){
-    findFromEnd(1).delink();
-  }
-  else{
-    findFromEnd(n+1).link(findFromEnd(n-1));
-  }
-}
-
-LNode(int a, LNode b){
-  value = a;
-  pointer = b;}
-  
-LNode(){
-  value = 0;
-  pointer = null;}
-  
-LNode(int a){
-  value = a;
-  pointer = null;}}
-  
