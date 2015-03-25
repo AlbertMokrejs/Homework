f10b8b95770d1c12b57fec5a23a50092647dcd9d
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-25 12:30:07 -0400

    Update MyDeque.java

--- 10_QueueBert/MyDeque.java
+++ 10_QueueBert/MyDeque.java
@@ -13,7 +13,9 @@ public class MyDeque<T>{
 	    resize();}
 	if(h == 0 && t == S.length -1;){
 	    resize();}
-	if(h == t){
+	if(h == t && S[h] == null){
+	    S[h] = x;}
+	if(h == t-1 && S[h] == null){
 	    S[h] = x;}
 	else{
 	    if(h!=0){
@@ -29,8 +31,10 @@ public class MyDeque<T>{
 	    resize();}
 	if(h == 0 && t == S.length -1;){
 	    resize();}
-	if(h == t){
+	if(h == t && S[t] == null){
 	    S[h] = x;}
+	if(h == t-1 && S[t] == null){
+	    S[t] = x;}
 	else{
 	    if(t!=length-1;){
 		S[t+1] = x;}
@@ -49,5 +53,46 @@ public class MyDeque<T>{
 	if(h > S.length-1){
 	    h = 0;}
 	return tmp;}
-	
-	}
+
+    public T removeLast(){
+	if(S[t] == null){
+	    throw new NoSuchElementException();}
+    T tmp = S[t];
+    S[t] = null;
+    t--;
+    if(t < 0){
+	h = s.length-1;}
+    return tmp;}
+
+    public T getFirst(){
+	if(S[h] == null){
+	    throw new NoSuchElementException();}
+	return S[h];}
+
+    public T getLast(){
+	is(S[t] == null){
+	    throw new NoSuchElementException();}
+	return S[t];}
+
+    public void resize(){
+	T[] z = new T[S.length *2];
+	int c = 0;
+	int x = h;
+	if(t < h){
+	    while(x < S.length){
+		T[c] = S[x];
+		x++;
+		c++;}
+	    x = 0;
+	    while(x < t+1){
+		T[x] = S[x];
+		x++;
+		c++;}}
+	if(h < t){
+	    while(x <= t){
+		T[c] = S[x];
+		x++;
+		c++;}}
+	S = T;}
+
+}
