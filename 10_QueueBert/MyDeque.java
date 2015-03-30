8e2b6eb3db89cdbd005cc12a6d320576e70a9fc6
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-29 21:07:02 -0400

    COMPLETED UPDATE
    Fixed bugs produced during last update as that update couldn't be compiled at the time it was written.
    
    Fixed trailing nulls in toString.

--- 10_QueueBert/MyDeque.java
+++ 10_QueueBert/MyDeque.java
@@ -6,21 +6,57 @@
 	    public int t;
 	    public Object[] S;
 	    public boolean TOGGLE;
+
+	    public static void main(String[]args){
+		MyDeque a = new MyDeque(true);
+		a.addFirst(5);
+		a.addFirst(6);
+		a.addLast(4);
+		a.addLast(3);
+		a.addLast(2);
+		a.addFirst(7);
+		System.out.println(a);
+		for(int x = 0; x < 240; x++){
+		    System.out.println(a);
+		    a.addLast(x%5);}
+		for(int y = 0; y < 240; y++){
+		    System.out.println(a);
+		    a.removeLast();}
+		System.out.println(a);}
+		
 	
 	    public MyDeque(){
 		S = new Object[16];
-		h = 0;
-		t = 0;
+		h = 4;
+		t = 4;
 	    	TOGGLE = true;
 	    }
 	    
 	    public MyDeque(boolean x){
 	    	S = new Object[16];
-	    	h = 0;
-	    	t = 0;
+	    	h = 4;
+	    	t = 4;
 	    	TOGGLE = x;
 	    }
 	    
+	    public String toString(){
+		String str = "";
+		int x = h;
+		if(h > t){
+		    while(x < S.length){
+			str += S[x];
+			x++;}
+		    x = 0;
+		    while(x <= t){
+			str += S[x];
+			x++;}}
+		else{
+		    while(x <= t){
+			str += S[x];
+			x++;}}
+		return str;}
+
+
 	    public void flop(){
 	    	TOGGLE = !TOGGLE;
 	    }
@@ -31,9 +67,11 @@
 		if(h == 0 && t == S.length -1){
 		    resize();}
 		if(h == t && S[h] == null){
-		    S[h] = x;}
+		    S[h] = x;
+		    h++;}
 		if(h == t-1 && S[h] == null){ //second add is a special case
-		    S[h] = x;}
+		    S[h] = x;
+		    h++;}
 		else{
 		    if(h!=0){
 			S[h-1] = x;}
@@ -49,9 +87,11 @@
 		if(h == 0 && t == S.length -1){
 		    resize();}
 		if(h == t && S[t] == null){
-		    S[h] = x;}
+		    S[t] = x;
+		    t--;}
 		if(h == t-1 && S[t] == null){ //special case
-		    S[t] = x;}
+		    S[t] = x;
+		    t--;}
 		else{
 		    if(t!=S.length-1){
 			S[t+1] = x;}
@@ -62,11 +102,12 @@
 		  
 	
 	    public T removeFirst(){
+		h++;
 		if(S[h] == null){
+		    h--;
 		    throw new NoSuchElementException();}
 	        T tmp = (T)S[h];
-		S[h] = null;
-		h++;
+		S[h-1] = null;
 		if(h > S.length-1){
 		    h = 0;}
 		if(S.length < 1000 && TOGGLE && size() <= (int)S.length/4){
@@ -75,13 +116,14 @@
 		return tmp;}
 	
 	    public T removeLast(){
+		t--;
 		if(S[t] == null){
+		    t++;
 		    throw new NoSuchElementException();}
 	    T tmp = (T)S[t];
-	    S[t] = null;
-	    t--;
+	    S[t+1] = null;
 	    if(t < 0){
-		h = s.length-1;}
+		t = S.length-1;}
 		if(S.length < 10000 && TOGGLE && size() <= (int)S.length/4){
 			desize();
 		}
@@ -98,7 +140,7 @@
 		return (T)S[t];}
 	
 	    public void resize(){
-		Object[] z = new Object[S.length *2];
+		Object[] T = new Object[S.length *2];
 		int c = 0;
 		int x = h;
 		if(t < h){
@@ -107,8 +149,8 @@
 			x++;
 			c++;}
 		    x = 0;
-		    while(x < t+1){
-			T[x] = S[x];
+		    while(x <= t){
+			T[c] = S[x];
 			x++;
 			c++;}}
 		if(h < t){
@@ -116,10 +158,12 @@
 			T[c] = S[x];
 			x++;
 			c++;}}
+		h = 0;
+		t = c-1;
 		S = T;}
 		
 		public void desize(){
-		Object[] z = new Object[(int)S.length/2];
+		Object[] T = new Object[(int)S.length/2];
 		int c = 0;
 		int x = h;
 		if(t < h){
@@ -128,8 +172,8 @@
 			x++;
 			c++;}
 		    x = 0;
-		    while(x < t+1){
-			T[x] = S[x];
+		    while(x <= t){
+			T[c] = S[x];
 			x++;
 			c++;}}
 		if(h < t){
@@ -137,6 +181,8 @@
 			T[c] = S[x];
 			x++;
 			c++;}}
+		h = 0;
+		t = c-1;
 		S = T;}
 		
 		public int size(){
@@ -147,7 +193,7 @@
 			x++;
 			c++;}
 		    x = 0;
-		    while(x < t+1){
+		    while(x <= t){
 			x++;
 			c++;}}
 		if(h < t){
