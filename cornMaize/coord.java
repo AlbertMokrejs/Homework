4c69d78950558178186645ac042c8700dfab3850
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-30 17:18:24 -0400

    Create coord.java

---
+++ cornMaize/coord.java
@@ -0,0 +1,22 @@
+public class coord{
+
+int x;
+int y;
+
+public coord(int a, int b){
+  x = a;
+  y = b;}
+  
+public getx(){
+  return x;}
+  
+public gety(){
+  return y;}
+
+public setx(int a){
+  x = a;}
+  
+public sety(int a){
+  y = a;}
+  
+}
