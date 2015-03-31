2edaf0c3e726e22ada3e33b64caa689b73738e06
Albert Mokrejs <albertmokrejs@gmail.com>
2015-03-31 18:39:31 -0400

    Untested Fix
    Attempting to resolve issues that lead to occasional issues with empty stacks being found. Not yet confirmed as fixed.

--- 11CornMaize/Maze.java
+++ 11CornMaize/Maze.java
@@ -1,8 +1,23 @@
+import java.util.*;
+
 public class Maze{
 
 ArrayDeque<Stack<coord>> deck;
 char[][] map;
 boolean solved;
+
+public static void main(String[]args){
+char[][] a = new char[5][5];
+a[0] = new char[]{'X','X','X','X','X'};
+a[1] = new char[]{'X','S','X',' ','X'};
+a[2] = new char[]{'X',' ',' ',' ','X'};
+a[3] = new char[]{'X',' ','X','E','X'};
+a[4] = new char[]{'X','X','X','X','X'};
+Maze x = new Maze(a);
+System.out.println(x);
+x.run();
+System.out.println(x);
+}
 
 public coord find(char a){
   coord tmp = new coord(0,0);
@@ -21,41 +36,46 @@ public Maze(char[][] x){
   Stack<coord> tmp = new Stack<coord>();
   map = x;
   tmp.push(find('S'));
+  tmp.push(find('S'));
   deck = new ArrayDeque<Stack<coord>>();
   deck.addLast(tmp);
 }
 
 public boolean check(coord a, coord b){
-  return map[a.getx()][a.gety()] != 'X' && ( a.getx() != b.getx() || a.gety() != b.gety() );
+    return a.getx() < map.length && a.getx() > -1 && a.gety() < map[0].length &&  a.gety() > -1 && map[a.getx()][a.gety()] != 'X' && ( a.getx() != b.getx() || a.gety() != b.gety() );
 }
 
 public void next(){
   Stack<coord> a = deck.removeFirst();
-  coord tmpb = a.pop();
-  coord tmpa = a.peek();
-  tmpa.setx(tmpa.getx()+1);
-  if(check(tmpa,tmpb)){
+  coord tmbz = a.peek();
+  coord tmby = a.peek();
+  coord tmpx = a.peek();
+  coord typw = a.peek();
+  coord tmpa = a.pop();
+  coord tmpb = a.peek();
+  a.push(tmpa);
+  tmpz.setx(tmpz.getx()+1);
+  if(check(tmpz,tmpb)){
     Stack<coord> tmps = (Stack<coord>)a.clone();
-    tmps.push(tmpa);
+    tmps.push(tmpz);
     deck.addLast(tmps);
   }
-  tmpa.setx(tmpa.getx()-2);
-  if(check(tmpa,tmpb)){
+  tmpy.setx(tmpy.getx()-1);
+  if(check(tmpy,tmpb)){
     Stack<coord> tmps = (Stack<coord>)a.clone();
-    tmps.push(tmpa);
+    tmps.push(tmpy);
     deck.addLast(tmps);
   }
-  tmpa.setx(tmpa.getx()+1);
-  tmpa.sety(tmpa.gety()+1);
-  if(check(tmpa,tmpb)){
+  tmpx.sety(tmpx.gety()+1);
+  if(check(tmpx,tmpb)){
     Stack<coord> tmps = (Stack<coord>)a.clone();
-    tmps.push(tmpa);
+    tmps.push(tmpx);
     deck.addLast(tmps);
   }
-  tmpa.sety(tmpa.gety()-2);
-  if(check(tmpa,tmpb)){
+  tmpw.sety(tmpw.gety()-1);
+  if(check(tmpw,tmpb)){
     Stack<coord> tmps = (Stack<coord>)a.clone();
-    tmps.push(tmpa);
+    tmps.push(tmpw);
     deck.addLast(tmps);
   }
 }
@@ -65,7 +85,7 @@ public boolean solution(coord a){
 }
 
 public void run(){
-  Stack<coord> tmp;
+    Stack<coord> tmp = new Stack<coord>();   
   while(!solved){
     tmp = deck.removeFirst();
     deck.addFirst(tmp);
