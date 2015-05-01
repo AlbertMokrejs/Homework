63b07c276ea56421cb435fc86871e8b152560e67
Albert Mokrejs <albertmokrejs@gmail.com>
2015-05-01 11:57:32 -0400

    Create BSTreeNode.java

---
+++ 12_ToTreeOrNotToTree/BSTreeNode.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+
+public class BSTreeNode<E extends Comparable> implements Comparable<BSTreeNode<E extends Comparable>> {
+
+    public boolean hasValue;
+    public boolean hasLeft;
+    public boolean hasRight;
+    public E value;
+    public BSTreeNode<E> Left;
+    public BSTreeNode<E> Right;
+    public int tally;
+
+    public int compareTo(BSTreeNode<E> x){
+	return value.compareTo((E)x.getValue());
+    }
+
+    public BSTreeNode(){
+	hasValue = false;
+	value = null;
+	hasLeft = false;
+	hasRight = false;
+	Left = null;
+	Right = null;
+	tally = 0;
+    }
+
+    public BSTreeNode(E x){
+	hasValue = true;
+	value = x;
+	hasLeft = false;
+	hasRight = false;
+	Left = null;
+	Right = null;
+	tally = 0;
+    }
+
+    public void setValue(E x){
+	hasValue = true;
+	value = x;
+    }
+
+    public void setTally(int x){
+	tally = x;}
+
+    public int getTally(){
+	return tally;}
+
+
+    public E getValue(){
+	if(hasValue){
+	    return value;
+	}
+	throw new NullPointerException();
+    }
+
+    public void setLeft(BSTreeNode<E> x){
+	hasLeft = true;
+	Left = x;
+    }
+
+    public void setRight(BSTreeNode<E> x){
+	hasRight = true;
+	Right = x;
+    }
+
+    public BSTreeNode<E> getLeft(){
+	if(hasLeft){
+	    return Left;
+	}
+	throw new NullPointerException();
+    }
+
+      public BSTreeNode<E> getRight(){
+	if(hasRight){
+	    return Right;
+	}
+	throw new NullPointerException();
+    }
+
+      public boolean lcheck(){
+	  hasLeft = (Left != null);
+	  return hasLeft;
+      }
+      
+      public boolean rcheck(){
+	  hasRight = (Right != null);
+	  return hasRight;
+      }
+
+      public boolean hasRoom(){
+	  return !(hasRight && hasLeft);
+      }
+
+      public boolean hasLeft(){
+	  return hasLeft;}
+
+      public boolean hasRight(){
+	  return hasRight;}
+
+      public String toString(){
+	  return "" + value;}
+
+
+}
